7db677f4493c94a6ac2ef258ea9158408d800659
Brandon Michels <bmichels09@gmail.com>
2021-05-31 03:02:31 -0500

    Update after 10.6.1

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def index():
 def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
-   mars.update({}, mars_data, upsert=True)
+   mars.replace_one({}, mars_data, upsert=True)
    return redirect('/', code=302)
 
 if __name__ == "__main__":
